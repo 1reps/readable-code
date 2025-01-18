643de70a1839b20a1bf06998b3515bd9c0bff4c3
1reps <kdm9982@gmail.com>
2025-01-18 20:18:08 +0900

    OCP:Open-Closed Principal

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -1,9 +1,20 @@
 package cleancode.minesweeper.tobe;
 
+import cleancode.minesweeper.tobe.gamelevel.Advanced;
+import cleancode.minesweeper.tobe.gamelevel.Beginner;
+import cleancode.minesweeper.tobe.gamelevel.GameLevel;
+import cleancode.minesweeper.tobe.gamelevel.Middle;
+import cleancode.minesweeper.tobe.gamelevel.VeryBeginner;
+
 public class GameApplication {
 
     public static void main(String[] args) {
-        Minesweeper minesweeper = new Minesweeper();
+        GameLevel veryBeginner = new VeryBeginner();
+        GameLevel beginner = new Beginner();
+        GameLevel middle = new Middle();
+        GameLevel advanced = new Advanced();
+
+        Minesweeper minesweeper = new Minesweeper(veryBeginner);
         minesweeper.run();
     }
 
